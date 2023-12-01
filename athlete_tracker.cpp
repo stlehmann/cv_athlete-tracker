f84619c09909f0ead9ef7b913997294c8561b74f
Stefan Lehmann <stlm@posteo.de>
2023-12-01 21:39:56 +0100

    Prettify / clarify

--- athlete_tracker.cpp
+++ athlete_tracker.cpp
@@ -77,21 +77,25 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Open output
+    // Extract relevant video properties
+    int frame_width = static_cast<int>(cap.get(CAP_PROP_FRAME_WIDTH));
+    int frame_height = static_cast<int>(cap.get(CAP_PROP_FRAME_HEIGHT));
+    int fps = static_cast<int>(cap.get(CAP_PROP_FPS));    
+    
+    cout << "Input video width: " << frame_width << "px, height: " << frame_height << "px, fps: " << fps << endl << endl;
+
+    // Open output 
     VideoWriter output;
-    if (vm.count("output-file")) {
-        int frame_width = static_cast<int>(cap.get(3));
-        int frame_height = static_cast<int>(cap.get(4));
+    if (vm.count("output-file")) {   
         Size frame_size(frame_width, frame_height);
-        int fps = 20;
         output = VideoWriter(vm["output-file"].as<string>(), VideoWriter::fourcc('M', 'J', 'P', 'G'), fps, frame_size);
     }
 
     // init the correlation tracker
     dlib::correlation_tracker tracker;
+    dlib::drectangle tracker_drect;
     bool tracker_started = false;
-    dlib::drectangle drect;
-    bool athlete_detected = false;
+    bool athlete_detected = false;  
 
     // Capture first frame
     while (cap.isOpened()) {
@@ -102,33 +106,39 @@ int main(int argc, char* argv[]) {
                 
         // Define a region of interest (ROI) for tracking        
         if (!athlete_detected) {
-            if (autodetect) {
-                int image_height = frame.rows;
-                int image_width = frame.cols;
 
+            // If autodetection has been enabled start the object detector and mark the object with the highest confidence for the
+            // class person
+            if (autodetect) {
                 // initialize the object detector
                 dnn::Net object_detector = dnn::readNetFromCaffe(MODEL_PROTOTXT, MODEL_MODEL);
                 
                 // Create a blob that will be passed to object detection model
-                Mat blob = dnn::blobFromImage(frame, SCALE, Size(image_width, image_height), 150);
+                Mat blob = dnn::blobFromImage(frame, SCALE, Size(frame_width, frame_height), 150);
                 object_detector.setInput(blob);
                 Mat detections = object_detector.forward();
 
                 // Process the output to extract object detections. We only want the one with the highest confidence
                 Mat detectionMat(detections.size[2], detections.size[3], CV_32F, detections.ptr<float>());
                 for (int i = 0; i < min(MAX_N_DETECTIONS, detectionMat.rows); ++i) {
+
+                    // extract confidence and label from detection matrix
                     float confidence = detectionMat.at<float>(i, 2);
                     string label = classes[static_cast<int>(detectionMat.at<float>(i, 1))];
 
-                    if (confidence > min_confidence && label == "person") {  // Adjust the confidence threshold as needed
-                        int left = static_cast<int>(detectionMat.at<float>(i, 3) * frame.cols);
-                        int top = static_cast<int>(detectionMat.at<float>(i, 4) * frame.rows);
-                        int right = static_cast<int>(detectionMat.at<float>(i, 5) * frame.cols);
-                        int bottom = static_cast<int>(detectionMat.at<float>(i, 6) * frame.rows);
-
-                        // fire up the correlation tracker
-                        drect = dlib::drectangle(left, top, right, bottom);
+                    if (label == "person" && confidence > min_confidence) {  // Adjust the confidence threshold as needed
+                        
+                        // set flag that athlete has been detected, object detector won't be triggered again
                         athlete_detected = true;
+
+                        // extract all four corners of the bounding box
+                        int left = static_cast<int>(detectionMat.at<float>(i, 3) * frame_width);
+                        int top = static_cast<int>(detectionMat.at<float>(i, 4) * frame_height);
+                        int right = static_cast<int>(detectionMat.at<float>(i, 5) * frame_width);
+                        int bottom = static_cast<int>(detectionMat.at<float>(i, 6) * frame_height);
+
+                        // Initialize the tracker rectangle
+                        tracker_drect = dlib::drectangle(left, top, right, bottom);
 
                         // draw rectangle
                         cv::rectangle(frame, cv::Point(left, top), cv::Point(right, bottom), cv::Scalar(0, 0, 255), 2);
@@ -136,15 +146,17 @@ int main(int argc, char* argv[]) {
                 }
             }
             else {
+                // If autodetect is not activate let the user manually create a rectangle around the athlete that shall be tracked
                 Rect2d r = selectROI(frame);
-                drect = dlib::drectangle(r.x, r.y, r.x+r.width, r.y+r.height);
+                // Initialize the tracker rectangle
+                tracker_drect = dlib::drectangle(r.x, r.y, r.x+r.width, r.y+r.height);
                 athlete_detected = true;
             }
         }
         else {
             if (!tracker_started) {
-                // start the tracker    
-                tracker.start_track(cv_image<rgb_pixel>(frame), drect);
+                // start the tracker if it hasn't been started, yet
+                tracker.start_track(cv_image<rgb_pixel>(frame), tracker_drect);
                 tracker_started = true;
             }
             else {
@@ -152,13 +164,13 @@ int main(int argc, char* argv[]) {
                 tracker.update(cv_image<rgb_pixel>(frame));
 
                 // Get the tracking result
-                dlib::drectangle trackedRect = tracker.get_position();
+                tracker_drect = tracker.get_position();
 
                 // Draw a rectangle around the tracked object
-                cv::rectangle(frame, cv::Point(trackedRect.left(), trackedRect.top()), cv::Point(trackedRect.right(), trackedRect.bottom()), cv::Scalar(0, 0, 255), 2);
+                cv::rectangle(frame, cv::Point(tracker_drect.left(), tracker_drect.top()), cv::Point(tracker_drect.right(), tracker_drect.bottom()), cv::Scalar(0, 0, 255), 2);
 
                 // Print coordinates
-                cout << "x=" << trackedRect.left() << ", y=" << trackedRect.top() << ", width=" << trackedRect.width() << ", height=" << trackedRect.height() << endl;
+                cout << "x=" << tracker_drect.left() << ", y=" << tracker_drect.top() << ", width=" << tracker_drect.width() << ", height=" << tracker_drect.height() << endl;
             }
         }
        
